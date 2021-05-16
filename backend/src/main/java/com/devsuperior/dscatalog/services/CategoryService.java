c6babc804cee18713bc180b9c2f219e70669988d
BernardoModerno <bernardo.moderno@gmail.com>
2021-05-16 19:46:01 -0300

    Banco de dados H2, camadas

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.List;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryResitory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryResitory repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+}
