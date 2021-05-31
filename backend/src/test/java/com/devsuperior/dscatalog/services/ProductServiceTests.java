346c61a04f565b3df7ba26e3d098fdddc8c0a169
BernardoModerno <bernardo.moderno@gmail.com>
2021-05-31 20:40:50 -0300

    ResourceNotFoundException quando id não existe

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -11,6 +11,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 
 @ExtendWith(SpringExtension.class)
@@ -32,6 +33,16 @@ public class ProductServiceTests {
 
 		Mockito.doNothing().when(repository).deleteById(existingId);
 		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+	}
+
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionDoNothingWhenIdDoesNotExists() {
+
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+
+		Mockito.verify(repository, Mockito.times(1)).deleteById(nonExistingId);
 	}
 
 	@Test
